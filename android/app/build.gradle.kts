d81a6985af44cc54937e39c5e5fbd01f8f7f7af9
Muhammed Anas <muhammadanaswayanad@gmail.com>
2025-05-07 15:04:09 +0530

    Add app icons and update macOS Podfile for Firebase integration
    - Added new app icons for iOS with different resolutions (72x72@1x, 72x72@2x).
    - Created a Podfile for macOS with Firebase dependencies and configurations.
    - Updated Podfile.lock to include Firebase and related dependencies.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,15 +1,21 @@
+import java.util.Properties
+import java.io.FileInputStream
+
 plugins {
     id("com.android.application")
-    // START: FlutterFire Configuration
-    id("com.google.gms.google-services")
-    // END: FlutterFire Configuration
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
 }
 
+val keystoreProperties = Properties()
+val keystorePropertiesFile = rootProject.file("key.properties")
+if (keystorePropertiesFile.exists()) {
+    keystoreProperties.load(FileInputStream(keystorePropertiesFile))
+}
+
 android {
-    namespace = "com.tijus_academy"
+    namespace = "com.learn.tijus_academy"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = "27.0.12077973"
 
@@ -24,25 +30,28 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.tijus_academy"
+        applicationId = "com.learn.tijus_academy"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
-        targetSdk = 33
-        versionCode = flutter.versionCode.toInt()
-        versionName = flutter.versionName.toString()
+        targetSdk = flutter.targetSdkVersion
+        versionCode = flutter.versionCode
+        versionName = flutter.versionName
+    }
+
+    signingConfigs {
+        create("release") {
+            keyAlias = keystoreProperties["keyAlias"] as String
+            keyPassword = keystoreProperties["keyPassword"] as String
+            storeFile = rootProject.file(keystoreProperties["storeFile"] as String)
+            storePassword = keystoreProperties["storePassword"] as String
+        }
     }
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
-            isMinifyEnabled = true
-             proguardFiles(
-                    getDefaultProguardFile("proguard-android-optimize.txt"),
-                    "proguard-rules.pro"
-            )
+            // Using release signing configuration
+            signingConfig = signingConfigs.getByName("release")
         }
     }
 }
---
+++ android/app/src/main/kotlin/com/learn/tijus_academy/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.learn.tijus_academy
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
